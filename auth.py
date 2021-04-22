32e9592d745be9f8e9f9d7ff3d88bd693e46b9d6
Rohan <rhnshiva@gmail.com>
2021-04-22 08:27:13 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -18,6 +18,7 @@ class Auth():
         payload = {
             'exp' : datetime.utcnow() + timedelta(days=0, minutes=30),
             'iat' : datetime.utcnow(),
+	    'scope': 'access_token',
             'sub' : username
         }
         return jwt.encode(
@@ -29,16 +30,19 @@ class Auth():
     def decode_token(self, token):
         try:
             payload = jwt.decode(token, self.secret, algorithms=['HS256'])
-            return payload['sub']
+            if (payload['scope'] == 'access_token'):
+                return payload['sub']   
+            raise HTTPException(status_code=401, detail='Scope for the token is invalid')
         except jwt.ExpiredSignatureError:
             raise HTTPException(status_code=401, detail='Token expired')
         except jwt.InvalidTokenError:
             raise HTTPException(status_code=401, detail='Invalid token')
 	    
     def encode_refresh_token(self, username):
-    	payload = {
+        payload = {
             'exp' : datetime.utcnow() + timedelta(days=0, hours=10),
             'iat' : datetime.utcnow(),
+	    'scope': 'refresh_token',
             'sub' : username
         }
         return jwt.encode(
@@ -49,10 +53,12 @@ class Auth():
     def refresh_token(self, refresh_token):
         try:
             payload = jwt.decode(refresh_token, self.secret, algorithms=['HS256'])
-	    username = payload['sub']
-	    new_token = self.encode_token(username)
-            return new_token
-	except jwt.ExpiredSignatureError:
+            if (payload['scope'] == 'refresh_token'):
+                username = payload['sub']
+                new_token = self.encode_token(username)
+                return new_token
+            raise HTTPException(status_code=401, detail='Invalid scope for token')
+        except jwt.ExpiredSignatureError:
             raise HTTPException(status_code=401, detail='Refresh token expired')
         except jwt.InvalidTokenError:
             raise HTTPException(status_code=401, detail='Invalid refresh token')
